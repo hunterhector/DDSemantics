849db448cd3fc21c2d99c1a0fbc6e340a0c6fc98
hunterhector <hunterhector@gmail.com>
2018-04-13 17:31:18 -0400

    Fix mapping.

---
+++ event/arguments/prepare/frame_collector.py
@@ -0,0 +1,127 @@
+import logging
+import os
+import operator
+from collections import Counter
+
+
+class ArgFrameCollector:
+    def __init__(self):
+        self.args_fe_map = {}
+        self.fe_args_map = {}
+        self.fe_count = Counter()
+        self.arg_count = Counter()
+        self.len_arg_fields = 5
+
+    def read_dataset(self, data_in):
+        batch = 10000
+        count = 0
+
+        logging.info("Reading: " + data_in)
+
+        num_format_errors = 0
+
+        with open(data_in) as data:
+            for line in data:
+                line = line.strip()
+                if line.startswith("#"):
+                    doc_name = line.rstrip("#")
+                    continue
+                elif line == "":
+                    continue
+
+                count += 1
+
+                fields = line.split("\t")
+
+                if len(fields) < 3:
+                    num_format_errors += 1
+                    continue
+
+                predicate, context, frame = fields[:3]
+
+                arg_fields = fields[3:-1]
+                for arg in [arg_fields[x:x + self.len_arg_fields] for x in
+                            range(0, len(arg_fields), self.len_arg_fields)]:
+                    if len(arg) < self.len_arg_fields:
+                        num_format_errors += 1
+                        continue
+
+                    prop, fe = arg[:2]
+
+                    prop_entry = (predicate, prop)
+                    fe_entry = (frame, fe)
+
+                    if not prop == "NA":
+                        self.arg_count[prop_entry] += 1
+
+                    if not fe == "NA":
+                        self.fe_count[fe_entry] += 1
+
+                    # if prop == "NA":
+                    #     if fe_entry not in self.fe_args_map:
+                    #         self.fe_args_map[fe_entry] = {}
+                    #     continue
+                    #
+                    # if fe == "NA":
+                    #     if prop_entry not in self.args_fe_map:
+                    #         self.args_fe_map[prop_entry] = {}
+                    #     continue
+
+                    from_prop = {}
+                    if prop_entry in self.args_fe_map:
+                        from_prop = self.args_fe_map[prop_entry]
+                    else:
+                        self.args_fe_map[prop_entry] = from_prop
+
+                    try:
+                        from_prop[fe_entry] += 1
+                    except KeyError:
+                        from_prop[fe_entry] = 1
+
+                    from_frame = {}
+
+                    if fe_entry in self.fe_args_map:
+                        from_frame = self.fe_args_map[fe_entry]
+                    else:
+                        self.fe_args_map[fe_entry] = from_frame
+
+                    try:
+                        from_frame[prop_entry] += 1
+                    except KeyError:
+                        from_frame[prop_entry] = 1
+
+                    if count % batch == 0:
+                        logging.info("%d lines processed, %d errors." % (
+                            count, num_format_errors))
+
+    def write(self, out_dir):
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+
+        with open(os.path.join(out_dir, 'args_frames.tsv'), 'w') as out:
+            for key, val in self.args_fe_map.items():
+                out.write("%s %s\t%s\n" % (
+                    ' '.join(key), self.arg_count[key], self.sorted_counts(val)
+                ))
+
+        with open(os.path.join(out_dir, 'frames_args.tsv'), 'w') as out:
+            for key, val in self.fe_args_map.items():
+                out.write("%s %s\t%s\n" % (
+                    ' '.join(key), self.fe_count[key], self.sorted_counts(val)
+                ))
+
+    @staticmethod
+    def sorted_counts(item_counts):
+        sorted_items = sorted(item_counts.items(), reverse=True,
+                              key=operator.itemgetter(1))
+        return ' '.join(["%s %d" % (' '.join(k), v) for k, v in sorted_items])
+
+
+if __name__ == '__main__':
+    import sys
+
+    logging.basicConfig(level=logging.INFO)
+
+    mapper = ArgFrameCollector()
+    mapper.read_dataset(sys.argv[1])
+    mapper.write(sys.argv[2])
--- event/arguments/prepare/frame_mapper.py
+++ event/arguments/prepare/frame_mapper.py
@@ -1,127 +1,156 @@
-import logging
-import os
+from nltk.corpus import framenet as fn
 import operator
+import sys
+import logging
 from collections import Counter
 
 
-class ArgFrameMapper:
+class FrameMapper:
     def __init__(self):
-        self.args_fe_map = {}
-        self.fe_args_map = {}
-        self.fe_count = Counter()
-        self.arg_count = Counter()
-        self.len_arg_fields = 5
+        self.h_ferel = {}
+        self.__load_frame_relation()
+        self.fe_mappings = {}
 
-    def read_dataset(self, data_in):
-        batch = 10000
-        count = 0
+        self.fe_counts = Counter()
 
-        logging.info("Reading: " + data_in)
+    def __load_frame_relation(self):
+        for rel in fn.fe_relations():
+            subFe = (rel.subFrame.name, rel.subFEName)
+            superFe = (rel.superFrame.name, rel.superFEName)
+            if rel.type.name in ['Subframe', 'Inheritance']:
+                self.h_ferel[subFe] = superFe
 
-        num_format_errors = 0
+    def write_frame_mapping(self, frame_mapping_out):
+        with open(frame_mapping_out, 'w') as out:
+            for fe, args in self.fe_mappings.items():
+                args_str = ["%s %s %d" % (arg[0], arg[1], arg[2]) for arg in
+                            args]
+                out.write('%s %s %d\t%s\n' % (
+                    fe[0], fe[1], self.fe_counts[fe], ' '.join(args_str)))
 
-        with open(data_in) as data:
-            for line in data:
-                line = line.strip()
-                if line.startswith("#"):
-                    doc_name = line.rstrip("#")
+    def get_mapping(self, mapping_file):
+        frame_args = {}
+        args_frames = {}
+        with open(mapping_file) as mapping:
+            for line in mapping:
+                fe_info, arg_info = line.split('\t')
+                arg_fields = arg_info.split()
+
+                frame_name, fe_name, fe_count = fe_info.split()
+                fe = (frame_name, fe_name)
+
+                args = []
+                for arg in [arg_fields[x:x + 3] for x in
+                            range(0, len(arg_fields), 3)]:
+                    raw_predicate, role, arg_count = arg
+                    args.append(arg)
+
+                frame_args[fe] = args
+        return frame_args, args_frames
+
+    def load_raw_mapping(self, fe_mapping_file):
+        not_found_fes = {}
+        with open(fe_mapping_file) as fe_mapping:
+            for line in fe_mapping:
+                parts = line.strip().split('\t')
+
+                if len(parts) < 2:
                     continue
-                elif line == "":
+
+                fe_info, arg_info = parts
+
+                fe_parts = fe_info.split()
+                frame_name = fe_parts[0]
+                fe_count = int(fe_parts[-1])
+                fe_name = "_".join(fe_parts[1:-1])
+
+                if frame_name == "NA" or fe_name == "NA":
                     continue
 
-                count += 1
+                fe = (frame_name, fe_name)
 
-                fields = line.split("\t")
+                seen_predicates = set()
+                self.fe_counts[fe] += fe_count
 
-                if len(fields) < 3:
-                    num_format_errors += 1
-                    continue
+                arg_fields = arg_info.split()
+                args = Counter()
+                for arg in [arg_fields[x:x + 3] for x in
+                            range(0, len(arg_fields), 3)]:
+                    raw_predicate, role, arg_count = arg
 
-                predicate, context, frame = fields[:3]
+                    pred_info = raw_predicate.split("_")
+                    pred = pred_info[0]
+                    if pred == 'not' and len(pred_info) > 1:
+                        pred = pred_info[1]
 
-                arg_fields = fields[3:-1]
-                for arg in [arg_fields[x:x + self.len_arg_fields] for x in
-                            range(0, len(arg_fields), self.len_arg_fields)]:
-                    if len(arg) < self.len_arg_fields:
-                        num_format_errors += 1
-                        continue
+                    if not role == 'NA':
+                        args[(pred, role)] += int(arg_count)
 
-                    prop, fe = arg[:2]
+                    seen_predicates.add(pred)
 
-                    prop_entry = (predicate, prop)
-                    fe_entry = (frame, fe)
+                sorted_args = sorted(args.items(), key=operator.itemgetter(1),
+                                     reverse=True)
+                sorted_args = [(pred, role, count) for ((pred, role), count) in
+                               sorted_args]
 
-                    if not prop == "NA":
-                        self.arg_count[prop_entry] += 1
+                if len(sorted_args) == 0:
+                    not_found_fes[fe] = seen_predicates
+                else:
+                    self.fe_mappings[fe] = sorted_args
 
-                    if not fe == "NA":
-                        self.fe_count[fe_entry] += 1
+        self.fill_blank(not_found_fes)
 
-                    # if prop == "NA":
-                    #     if fe_entry not in self.fe_args_map:
-                    #         self.fe_args_map[fe_entry] = {}
-                    #     continue
-                    #
-                    # if fe == "NA":
-                    #     if prop_entry not in self.args_fe_map:
-                    #         self.args_fe_map[prop_entry] = {}
-                    #     continue
+    def use_parent_syntax(self, fe, target_pred):
+        # fe_name = (frame, fe)
+        if fe in self.h_ferel:
+            super_fe = self.h_ferel[fe]
+            if super_fe in self.fe_mappings:
+                arg_roles = Counter()
+                for predicate, arg, count in self.fe_mappings[super_fe]:
+                    arg_roles[arg] += 1
+                    if target_pred == predicate:
+                        return arg
 
-                    from_prop = {}
-                    if prop_entry in self.args_fe_map:
-                        from_prop = self.args_fe_map[prop_entry]
-                    else:
-                        self.args_fe_map[prop_entry] = from_prop
+                sorted_roles = sorted(arg_roles.items(), reverse=True,
+                                      key=operator.itemgetter(1))
+                most_role = sorted_roles[0][0]
+                return most_role
+            else:
+                return self.use_parent_syntax(super_fe, target_pred)
+        return None
 
-                    try:
-                        from_prop[fe_entry] += 1
-                    except KeyError:
-                        from_prop[fe_entry] = 1
+    def fill_blank(self, not_found_fes):
+        print("%d frame elements not directly mapped." % len(not_found_fes))
 
-                    from_frame = {}
+        not_mapped = []
 
-                    if fe_entry in self.fe_args_map:
-                        from_frame = self.fe_args_map[fe_entry]
-                    else:
-                        self.fe_args_map[fe_entry] = from_frame
+        for (frame_name, fe_name), predicates in not_found_fes.items():
+            args = []
+            found_mapping = False
+            for predicate in predicates:
+                arg = self.use_parent_syntax(
+                    (frame_name, fe_name), predicate
+                )
+                if arg:
+                    args.append((predicate, arg, 0))
+                    found_mapping = True
 
-                    try:
-                        from_frame[prop_entry] += 1
-                    except KeyError:
-                        from_frame[prop_entry] = 1
+            if not found_mapping:
+                not_mapped.append((frame_name, fe_name))
 
-                    if count % batch == 0:
-                        logging.info("%d lines processed, %d errors." % (
-                            count, num_format_errors))
+        not_mapped_frames = set([t[0] for t in not_mapped])
 
-    def write(self, out_dir):
-        if not os.path.exists(out_dir):
-            os.makedirs(out_dir)
-
-        with open(os.path.join(out_dir, 'args_frames.tsv'), 'w') as out:
-            for key, val in self.args_fe_map.items():
-                out.write("%s %s\t%s\n" % (
-                    ' '.join(key), self.arg_count[key], self.sorted_counts(val)
-                ))
-
-        with open(os.path.join(out_dir, 'frames_args.tsv'), 'w') as out:
-            for key, val in self.fe_args_map.items():
-                out.write("%s %s\t%s\n" % (
-                    ' '.join(key), self.fe_count[key], self.sorted_counts(val)
-                ))
-
-    @staticmethod
-    def sorted_counts(item_counts):
-        sorted_items = sorted(item_counts.items(), reverse=True,
-                              key=operator.itemgetter(1))
-        return ' '.join(["%s %d" % (' '.join(k), v) for k, v in sorted_items])
+        print("After parent, %d FE in %d frames not mapped" % (
+            len(not_mapped), len(not_mapped_frames)))
 
 
 if __name__ == '__main__':
-    import sys
-
     logging.basicConfig(level=logging.INFO)
 
-    mapper = ArgFrameMapper()
-    mapper.read_dataset(sys.argv[1])
-    mapper.write(sys.argv[2])
+    frame_mapping_file = sys.argv[1]
+    frame_mapping_out = sys.argv[2]
+    arg_mapping_out = sys.argv[3]
+
+    compressor = FrameMapper()
+    compressor.load_raw_mapping(frame_mapping_file)
+    compressor.write_frame_mapping(frame_mapping_out)
