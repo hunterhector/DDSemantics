8ff30090d800dfa674a4d4e25d713e72e43548ff
hunterhector <hunterhector@gmail.com>
2018-07-01 17:55:21 -0400

    Structure.

---
+++ event/io/dataset.py
@@ -0,0 +1,19 @@
+import os
+
+
+class EventMention:
+    def __init__(self):
+        pass
+
+
+class EntityMention:
+    def __init__(self):
+        pass
+
+
+class DEDataset:
+    def __init__(self):
+        pass
+
+    def load_file(self):
+        pass
