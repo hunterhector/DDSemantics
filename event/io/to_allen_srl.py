870fb02ccd1581d08abf7075d01c2690e3179c51
hunterhector <hunterhector@gmail.com>
2018-09-03 14:01:30 -0400

    One single output file for simplicity.

--- event/io/to_allen_srl.py
+++ event/io/to_allen_srl.py
@@ -14,7 +14,6 @@ def convert(docid, inf, sent, out):
                      'start': start, 'end': end}
         out.write(json.dumps(sent_json))
         out.write('\n')
-
     out.write('\n')
 
 
@@ -22,11 +21,11 @@ if __name__ == '__main__':
     txt_dir = sys.argv[1]
     out_file = sys.argv[2]
 
-    for fname in os.listdir(txt_dir):
-        if fname.endswith('txt'):
-            docid = fname.replace('.txt', '')
-            sent_file = fname.replace('.txt', '.sent')
-            with open(os.path.join(txt_dir, fname)) as inf, open(
-                    os.path.join(txt_dir, sent_file)) as sent, open(
-                out_file, 'w') as out:
-                convert(docid, inf, sent, out)
+    with open(out_file, 'w') as out:
+        for fname in os.listdir(txt_dir):
+            if fname.endswith('txt'):
+                docid = fname.replace('.txt', '')
+                sent_file = fname.replace('.txt', '.sent')
+                with open(os.path.join(txt_dir, fname)) as inf, open(
+                        os.path.join(txt_dir, sent_file)) as sent:
+                    convert(docid, inf, sent, out)
