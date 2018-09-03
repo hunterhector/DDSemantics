fa1e110802929dbcfff69eeb2e7e0df51db2e06b
hunterhector <hunterhector@gmail.com>
2018-09-03 13:44:03 -0400

    Just one 's'

---
+++ event/io/to_allen_srl.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import json
+
+
+def convert(inf, sent, out):
+    for line in inf:
+        sent_json = {'sentence': line.strip()}
+        out.write(sent_json)
+
+    out.write('\n')
+
+
+if __name__ == '__main__':
+    txt_dir = sys.argv[1]
+    out_dir = sys.argv[2]
+
+    for fname in os.listdir(txt_dir):
+        if fname.endswith('txt'):
+            sent_file = fname.replace('.txt', '.sent')
+            with open(os.path.join(txt_dir, fname)) as inf, open(
+                    os.path.join(txt_dir, sent_file)) as sent, open(
+                os.path.join(out_dir, fname), 'w') as out:
+                convert(inf, sent, out)
