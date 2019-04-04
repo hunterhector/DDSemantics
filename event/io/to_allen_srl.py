6e28b1ecbab6fb6a1496c2fea8f6d4ee717b517a
hunterhector <hunterhector@gmail.com>
2019-04-04 15:27:15 -0400

    Fix SRL typo.

--- event/io/to_allen_srl.py
+++ event/io/to_allen_srl.py
@@ -12,8 +12,9 @@ def convert(docid, inf, sent, out):
         end = int(end)
         sent_json = {'sentence': txt[start: end], 'docid': docid,
                      'start': start, 'end': end}
-        out.run(json.dumps(sent_json))
-        out.run('\n')
+        out.write(json.dumps(sent_json))
+        out.write('\n')
+
 
 if __name__ == '__main__':
     txt_dir = sys.argv[1]
