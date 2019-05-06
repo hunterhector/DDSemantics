4ed0a10bb5c75e003b7cbfe35fe09c880a32f30f
hunterhector <hunterhector@gmail.com>
2019-05-05 23:08:37 -0400

    fix script.

--- scripts/hash_train_all_frames.sh
+++ scripts/hash_train_all_frames.sh
@@ -5,7 +5,7 @@ mkdir -p ${implicit_corpus}/gigaword_frames/hashed
 for f in ${implicit_corpus}/gigaword_frames/nyt_all_frames_shuffled/*.gz
 do
     if [[ -f ${f} ]]; then
-        h=${f//nyt_events_shuffled/hashed}
+        h=${f//nyt_all_frames_shuffled/hashed}
         echo 'Hashing '${f}' into '${h}
         python -m event.arguments.prepare.hash_cloze_data conf/implicit/hash_all_frames.py --HashParam.raw_data=${f} --HashParam.output_path=${h}
     fi
