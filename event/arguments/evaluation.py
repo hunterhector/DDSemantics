221dbe2516016b706ebb7eb99233b4c490e132f5
hunterhector <hunterhector@gmail.com>
2019-05-18 00:52:47 -0400

    stuff.

--- event/arguments/evaluation.py
+++ event/arguments/evaluation.py
@@ -144,8 +144,9 @@ class ImplicitEval:
 
                     if sel_name == 'neighbor':
                         print(meta)
+                        print(score, label)
 
-            input('these are close by entities')
+        input('these are close by entities')
 
         if ins_meta['has_true']:
             self.overall_res['num_fillable'] += 1
