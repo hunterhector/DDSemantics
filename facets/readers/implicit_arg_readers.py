cabc114face6be971427d53d2c2f201b5ccda48d
hunterhector <hunterhector@gmail.com>
2023-01-16 23:45:10 -0500

    stuff

---
+++ facets/readers/implicit_arg_readers.py
@@ -0,0 +1,11 @@
+"""
+Readers for implicit argument datasets:
+  - ON5V https://aclanthology.org/W13-0211/
+  - G&C
+  - RAMS https://nlp.jhu.edu/rams/
+"""
+from forte.data.base_reader import PackReader
+
+
+class RamsReader(PackReader):
+    pass
--- facets/readers/implict_reader.py
+++ facets/readers/implict_reader.py
@@ -2,10 +2,12 @@
 Process implicit argument datasets.
 """
 from typing import Any, Iterator
+import json
 
 from forte.data import DataPack, Span
 from forte.data.base_pack import PackType
 from forte.data.base_reader import PackReader
+from forte.processors.writers import PackIdJsonPackWriter
 
 from event.io.dataset.nombank import NomBank
 from event.io.dataset.base import Corpus, DEDocument
@@ -28,7 +30,23 @@ class GCNombankReader(PackReader):
             "implicit_path": "~/Documents/projects/data/implicit",
             # https://nlp.cs.nyu.edu/meyers/nombank/nombank.1.0.zip
             "nombank_path": "~/Documents/projects/data/nombank",
+            "wsj_path": "~/Document/projects/data/wsj",
         }
 
 
-PackIdJsonPackWriter
+class RamsReader(PackReader):
+
+    def _collect(self) -> Iterator[Any]:
+        with open(self.configs.rams_json_path) as rams_file:
+            root = json.load(rams_file)
+            root["ent_spans"]
+
+    def _parse_pack(self, collection: Any) -> Iterator[PackType]:
+        pass
+
+    @classmethod
+    def default_configs(cls):
+        return {
+            # https://nlp.jhu.edu/rams/
+            "rams_json_path": "~/Documents/projects/data/RAMS_1.0/data/train.jsonlines",
+        }
